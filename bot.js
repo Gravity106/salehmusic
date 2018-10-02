a80927af9bea547a120f7304bc3a688fe88a1206
Gravity106 <43704049+Gravity106@users.noreply.github.com>
2018-10-02 04:00:36 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,50 @@
+
+
+
+
+
+client.on('ready', () => {
+
+    console.log(`Logged in as ${client.user.tag}!`);
+
+    console.log(`in ${client.guilds.size} servers `)
+
+    console.log(`[NAWAF] ${client.users.size}`)
+
+    client.user.setStatus("online")
+
+});
+
+
+client.on("roleDelete", 'role' => {
+  client.setTimeout(() => {
+    role.guild.fetchAuditLogs({
+        limit: 1,
+        type: 30
+      })
+      .then(audit => {
+        let exec = audit.entries.map(a => a.executor.username)
+        try {
+
+             role.guild.fetchAuditLogs().then(logs => {
+        var userID = logs.entries.first().executor.id;
+        var userAvatar = logs.entries.first().executor.avatarURL;
+ 
+        let roleDelete = new Discord.RichEmbed()
+        .setTitle('**[ROLE DELETE]**')
+        .setThumbnail(userAvatar)
+        .setDescription(`**\n**:white_check_mark: Successfully \`\`DELETE\`\` Role.\n\n**Role Name:** \`\`${role.name}\`\` (ID: ${role.id})\n**By:** <@${userID}> (ID: ${userID})`)
+        .setColor('RED')
+        .setTimestamp()
+        .setFooter(role.guild.name, role.guild.iconURL)
+ 
+        logChannel.send(roleDelete);
+    })
+    
+ 
+ 
+ 
+ 
+ 
+ 
+ client.login(process.env.BOT_TOKEN);
